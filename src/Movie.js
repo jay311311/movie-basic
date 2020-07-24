767c67ece8ce5c524e7ba578c44e39a3383508a9
jay311311 <jay311311@gmail.com>
2020-07-25 00:13:44 +0900

    react에서 사용될 html뼈대 구축

--- src/Movie.js
+++ src/Movie.js
@@ -1,11 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Movie.css";
 
 function Movie({id, year, title, summary,poster}) {
     return(
-        <h4>
-            {title}
-        </h4>
+        <div class="movie">
+            <img src={poster} alt={title} title={title} ></img>
+            <div class="movie_data">
+                <h3 class="movie_title">{title}</h3>
+                <h5 class="movie_year">{year}</h5>
+                <p class="movie_summary">{summary}</p>
+            </div>
+        </div>
     )
 }
 
