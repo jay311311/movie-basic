0b66af0d44fb6f99358fd9a244c1e6cd1ce785ed
jay311311 <jay311311@gmail.com>
2020-07-26 23:09:06 +0900

    react 에 genres 와 runtime 추가

--- src/Movie.js
+++ src/Movie.js
@@ -2,14 +2,21 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./Movie.css";
 
-function Movie({id, year, title, summary,poster}) {
+function Movie({id, year, title, summary,poster, genres, runtime}) {
     return(
-        <div class="movie">
+        <div className="movie">
             <img src={poster} alt={title} title={title} ></img>
-            <div class="movie_data">
-                <h3 class="movie_title">{title}</h3>
-                <h5 class="movie_year">{year}</h5>
-                <p class="movie_summary">{summary}</p>
+            <div className="movie_data">
+                <h3 className="movie_title">{title}</h3>
+                <h5 className="movie_year">{year}</h5>
+                <ul className="genre">
+                    {genres.map((genre, index)=>
+                        <li key={index} className="genres_genre">{index}{genre}</li>
+                    )}
+                </ul>
+                    <p className="runtime">{runtime}</p>
+                <p className="movie_summary">{summary}</p>
+                
             </div>
         </div>
     )
@@ -20,7 +27,9 @@ Movie.propTypes = {
     year: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     summary:PropTypes.string.isRequired,
-    poster:PropTypes.string.isRequired
+    poster:PropTypes.string.isRequired,
+    genres:PropTypes.arrayOf(PropTypes.string).isRequired,
+    runtime:PropTypes.number.isRequired
     //데이터 key 는 medium_cover_image라고 되어 있음, props를 가져올때 poster로 지정됨
 }
 
